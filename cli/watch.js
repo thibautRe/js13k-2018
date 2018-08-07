82d4345bdeebda3e61d619b42492461de8213125
Thibaut REMY <thibaut@thibaut.re>
2018-08-07 22:43:58 +0200

    :nail_care: Improve watcher when babel errors

--- cli/watch.js
+++ cli/watch.js
@@ -12,6 +12,7 @@ const { addStat } = require('./stats')
 
 // Keep track of if the initial scan has been performed already
 let isReady = false
+let hasInitErrored = false
 
 const devZipBuild = () => {
   logUpdate(chalk.gray('Rebuilding zip file...'))
@@ -25,23 +26,43 @@ const devZipBuild = () => {
 // Compiles files in src/app to src/app-build/
 const devRebuild = file => {
   logUpdate(chalk.gray('Compiling files for DEV...'))
-  const { code } = babel.transformFileSync(file, { ast: false })
-  // Create dirs recursively
-  const buildFile = file.replace('src/app', 'src/app-build')
-  fs.ensureDirSync(path.dirname(buildFile))
-  fs.writeFileSync(buildFile, code)
+  try {
+    const { code } = babel.transformFileSync(file, { ast: false })
 
-  // Do not perform full rebuild on the initial scan
-  if (!isReady) return
-  devZipBuild()
+    // Create dirs recursively
+    const buildFile = file.replace('src/app', 'src/app-build')
+    fs.ensureDirSync(path.dirname(buildFile))
+    fs.writeFileSync(buildFile, code)
+
+    // Do not perform full rebuild on the initial scan
+    if (!isReady) return
+    devZipBuild()
+  } catch (e) {
+    // Babel compilation error
+    if (e._babel) {
+      hasInitErrored = true
+
+      // Show Babel frame error
+      /* eslint-disable no-console */
+      console.error(e.message)
+      console.error(e.codeFrame)
+      /* eslint-enable no-console */
+
+      addStat('error')
+      return
+    }
+
+    throw e
+  }
 }
 
 module.exports = () => {
   const devWatcher = chokidar.watch('src/app/', {})
   devWatcher.on('add', devRebuild)
   devWatcher.on('change', devRebuild)
+  devWatcher.on('unlink', devZipBuild)
   devWatcher.on('ready', () => {
-    devZipBuild()
+    if (!hasInitErrored) devZipBuild()
     isReady = true
   })
 }
