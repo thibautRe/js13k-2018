1c4473a6b2f6b18e2e8baf38f33e1354480498a6
Thibaut REMY <thibaut@thibaut.re>
2018-08-07 22:56:50 +0200

    :nail_care: Handle stats read/write when file is odd

--- cli/stats.js
+++ cli/stats.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 const fs = require('fs-extra')
+const chalk = require('chalk')
 
 const fileName = 'stats.log'
 const fileNameBak = 'stats.bak.log'
@@ -8,14 +9,22 @@ let backupCounter = 0
 
 const getStats = () => {
   try {
-    return fs.readJsonSync(fileName)
+    const content = fs.readFileSync(fileName)
+    // If the file is empty, return empty array
+    if (content.toString() === '') {
+      return []
+    }
+    return JSON.parse(content)
   } catch (e) {
     // File doesnt exist is gracefully handled
     if (e.code === 'ENOENT') {
       return []
     }
-
-    throw e
+    // eslint-disable-next-line no-console
+    console.error(e.type)
+    // eslint-disable-next-line no-console
+    console.error(chalk.red('stats log file badly formed'))
+    return null
   }
 }
 
@@ -31,6 +40,9 @@ const saveStats = stats => {
 
 const addStat = stat => {
   const stats = getStats()
+
+  // Do not overwrite the stats file
+  if (stats === null) return
 
   // Only save if the previous one is different
   if (!stats.length || stats[stats.length - 1] !== stat) {
