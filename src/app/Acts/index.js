1f02e317b5d4f6be1ff309c8bb7efcf7ff46ba46
Thibaut REMY <thibaut@thibaut.re>
2018-08-23 22:33:06 +0200

    :pencil: Write down complete new story

--- src/app/Acts/index.js
+++ src/app/Acts/index.js
@@ -6,22 +6,46 @@
   const allActs = {
     Intro: [
       '',
-      '2Hello.',
-      '2How are you?',
-      "Pretty okay:Intro2|I'm alright:Intro2",
+      '2Man: Hello and welcome!',
+      '2Please... Come on in!',
+      '2Would you like something to drink?',
+      'Tea, please:Intro2|Coffee is fine:Intro2',
     ],
     Intro2: [
-      "3Alright. That's good.",
-      '2I guess...',
-      '2 ...',
-      '2 ...Anyway.',
-      'Welcome!',
-      'Where am I?:IntroWhere|Who are you?:NotImplemented',
+      "2Perfect, I'll go get that for you.",
+      '4You can wait in the living room in the meantime.',
+      '3%3You go in the living room and find a cat sitting in a couch%3',
+      'Pet the cat:Intro3',
     ],
-    IntroWhere: [
-      'Really?',
-      'Are you %2really%2 asking me this?',
-      'Sorry:NotImplemented',
+    Intro3: [
+      '2%3The cat likes the petting%3',
+      'Pet the cat:IntroCat1|Stop petting the cat:IntroCatStop',
+    ],
+    IntroCat1: [
+      '2%3The cat %3%2loves%2%3 the petting%3.',
+      'Pet the cat:IntroCat2|Stop petting the cat:IntroCatStop',
+    ],
+    IntroCat2: [
+      '3%3The cat gets up and goes away, visibly annoyed by too much petting.%3',
+      'Sit in the couch:IntroManQuestions',
+    ],
+    IntroCatStop: [
+      '2%3The cat goes back to sleep.%3',
+      '2%3You feel like the cat wanted more.%3',
+      'Leave the cat alone:IntroManQuestions',
+    ],
+    IntroManQuestions: [
+      '2Man: Ah you are there!',
+      '4I see that you got to meet with my cat, %0Mousti%0.',
+      "3He's very sweet, and really loves having company.",
+      "2Great. Now that you're here, shall we start?",
+      'Start what?:IntroWhat|Who are you?:NotImplemented',
+    ],
+    IntroWhat: [
+      '4Well, start the big project that we talked about of course!',
+      '3Our big project of %0deleting the Internet%0.',
+      "2You didn't forget, did you?",
+      'Of course not:NotImplemented',
     ],
     NotImplemented: ['Not implemented', 'Back to start:Intro'],
   }
